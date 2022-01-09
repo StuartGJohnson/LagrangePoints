894c7b8f1cb73d61b6a82c98eade6ab382e2f586
sjohnson <stuart.g.johnson@gmail.com>
2022-01-08 23:57:45 -0800

    Update plotting and check in figures.

--- plot_force.m
+++ plot_force.m
@@ -1,10 +1,15 @@
-function [x, y, f_x, f_y, f_amp] = plot_force(r0, r1, n, rmax)
+function [x, y, f_x, f_y, f_amp] = plot_force(r0, r1, n, r_range)
 % compute and plot the force on a massless particle in uniform cicular motion
 % in the gravity field of two orbiting masses (i.e. orbiting about the COM 
 % just as the two masses are.
 % sample the area from -rmax:rmax with n x n points
-dr_mesh = (2*rmax)/(n-1);
-[x, y] = meshgrid(-rmax:dr_mesh:rmax, -rmax:dr_mesh:rmax);
+rdiffx = r_range(2)-r_range(1);
+rdiffy = r_range(4)-r_range(3);
+dr_meshx = rdiffx/(n-1);
+dr_meshy = rdiffy/(n-1);
+x_sample = r_range(1):dr_meshx:r_range(2);
+y_sample = r_range(3):dr_meshy:r_range(4);
+[x, y] = meshgrid(x_sample, y_sample);
 r0_x = -ones(size(x))*r0;
 r0_y = zeros(size(y));
 r1_x = ones(size(x))*r1;
@@ -20,5 +25,7 @@ denom_1 = denom_1.*denom_1.*denom_1;
 f_x = x - r1*(r0+r1)^2 * r_r0_x ./denom_0 - r0*(r0+r1)^2 * r_r1_x ./ denom_1;
 f_y = y - r1*(r0+r1)^2 * r_r0_y ./denom_0 - r0*(r0+r1)^2 * r_r1_y ./ denom_1;
 f_amp = sqrt(f_x.*f_x + f_y.*f_y);
-contour(x,y,log(f_amp));
+% return m0-centric coordinates/plot
+x = x + r0;
+contour(x,y,log10(f_amp));
 end
--- read_me.m
+++ read_me.m
@@ -7,12 +7,17 @@
 r0= 1/82.3; r1 = 81.3/82.3;
 [xz,yz]=solve_force(r0,r1);
 % in m0-centric coordinates, these points are (x):
-xz + r0
-figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,1.8);
+xz = xz + r0
+figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,[-1.8 1.8 -1.8 1.8]);
 hold on
 plot(xz,yz, 'r+')
 axis equal
-title('earth-moon lagrange points')
+grid
+title('earth(m0)-moon(m1) lagrange points (m0/m1 = 81.3)')
+xlabel('normalized system x (x0 = 0, x1 = 1)')
+ylabel('normalized system y (y0 = 0)')
+legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
+print -dpng earth_moon.png
 
 % sun-moon is:
 % r0*m0 = r1*m1; r0+r1=1
@@ -20,35 +25,60 @@ title('earth-moon lagrange points')
 r0= 1/330001; r1 = 330000/330001;
 [xz,yz]=solve_force(r0,r1);
 % in m0-centric coordinates, these points are (x):
-xz + r0
-figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,1.8);
+xz = xz + r0
+figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,[-1.8 1.8 -1.8 1.8]);
 hold on
 plot(xz,yz, 'r+')
 axis equal
-title('sun-moon lagrange points')
+grid
+title('sun(m0)-earth(m1) lagrange points (m0/m1 = 330000)')
+xlabel('normalized system x (x0 = 0, x1 = 1)')
+ylabel('normalized system y (y0 = 0)')
+legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
+print -dpng sun_earth.png
+% and a zoom
+figure();[x,y,fx,fy,f]=plot_force(r0,r1,500,[0.98 1.02 -0.02 0.02]);
+hold on
+axis([0.98 1.02 -0.02 0.02])
+plot(xz,yz, 'r+')
+axis([0.98 1.02 -0.02 0.02])
+grid
+title('sun(m0)-earth(m1) lagrange points (m0/m1 = 330000)')
+xlabel('normalized system x (x0 = 0, x1 = 1)')
+ylabel('normalized system y (y0 = 0)')
+legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
+print -dpng sun_earth_zoom.png
 
 % bogus system 1;
 % mo/m1 = 5, so...
 r0=1/6; r1=5/6;
 [xz,yz]=solve_force(r0,r1);
 % in m0-centric coordinates, these points are (x):
-xz + r0
-figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,1.8);
+xz = xz + r0
+figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,[-1.8 1.8 -1.8 1.8]);
 hold on
 plot(xz,yz, 'r+')
 axis equal
-title('bogus system lagrange points (m0/m1 = 5)')
+grid
+title('fictitious system 1 lagrange points (m0/m1 = 5)')
+xlabel('normalized system x (x0 = 0, x1 = 1)')
+ylabel('normalized system y (y0 = 0)')
+legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
+print -dpng fict1.png
 
 % bogus system 2;
 % mo/m1 = 1, so...
 r0=1/2; r1=1/2;
 [xz,yz]=solve_force(r0,r1);
 % in m0-centric coordinates, these points are (x):
-xz + r0
-figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,1.8);
+xz = xz + r0
+figure();[x,y,fx,fy,f]=plot_force(r0,r1,1000,[-1.8 1.8 -1.8 1.8]);
 hold on
 plot(xz,yz, 'r+')
 axis equal
-title('bogus system lagrange points (m0/m1 = 1)')
-
-
+grid
+title('fictitious system 2 lagrange points (m0/m1 = 1)')
+xlabel('normalized system x (x0 = 0, x1 = 1)')
+ylabel('normalized system y (y0 = 0)')
+legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
+print -dpng fict2.png
