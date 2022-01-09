07d9be925bcbae92c59cb12757fcb8dc3c732131
sjohnson <stuart.g.johnson@gmail.com>
2022-01-09 00:06:52 -0800

    Fix y axis labels. regen plots.

--- read_me.m
+++ read_me.m
@@ -15,7 +15,7 @@ axis equal
 grid
 title('earth(m0)-moon(m1) lagrange points (m0/m1 = 81.3)')
 xlabel('normalized system x (x0 = 0, x1 = 1)')
-ylabel('normalized system y (y0 = 0)')
+ylabel('normalized system y (y0 = 0, y1 = 0)')
 legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
 print -dpng earth_moon.png
 
@@ -33,7 +33,7 @@ axis equal
 grid
 title('sun(m0)-earth(m1) lagrange points (m0/m1 = 330000)')
 xlabel('normalized system x (x0 = 0, x1 = 1)')
-ylabel('normalized system y (y0 = 0)')
+ylabel('normalized system y (y0 = 0, y1 = 0)')
 legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
 print -dpng sun_earth.png
 % and a zoom
@@ -45,7 +45,7 @@ axis([0.98 1.02 -0.02 0.02])
 grid
 title('sun(m0)-earth(m1) lagrange points (m0/m1 = 330000)')
 xlabel('normalized system x (x0 = 0, x1 = 1)')
-ylabel('normalized system y (y0 = 0)')
+ylabel('normalized system y (y0 = 0, y1 = 0)')
 legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
 print -dpng sun_earth_zoom.png
 
@@ -62,7 +62,7 @@ axis equal
 grid
 title('fictitious system 1 lagrange points (m0/m1 = 5)')
 xlabel('normalized system x (x0 = 0, x1 = 1)')
-ylabel('normalized system y (y0 = 0)')
+ylabel('normalized system y (y0 = 0, y1 = 0)')
 legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
 print -dpng fict1.png
 
@@ -79,6 +79,6 @@ axis equal
 grid
 title('fictitious system 2 lagrange points (m0/m1 = 1)')
 xlabel('normalized system x (x0 = 0, x1 = 1)')
-ylabel('normalized system y (y0 = 0)')
+ylabel('normalized system y (y0 = 0, y1 = 0)')
 legend({'contours of $\log{|\vec{f}|}$', 'zeros of $\vec{f}$'}, 'Interpreter', 'latex')
 print -dpng fict2.png
